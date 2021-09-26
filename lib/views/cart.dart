4457c796c60e546e23959e7ec29d8fa041f84732
ajomuch92 <aaronmontes_92@yahoo.es>
2021-09-26 09:55:13 -0600

    Agregando funcionalidad a la vista de carrito

--- lib/views/cart.dart
+++ lib/views/cart.dart
@@ -11,12 +11,15 @@ class Cart extends StatefulWidget {
 
 class _CartState extends State<Cart> {
   List<Product> _products = [];
+  List<Product> _allProducts = [];
 
   void _fillProducts(BuildContext context) {
-    List<Product> _allProducts = ModalRoute.of(context)!.settings.arguments as List<Product>;
-    setState(() {
-      _products = _allProducts.where((element) => element.isAdded!).toList();
-    });
+    if (_products.isEmpty && _allProducts.isEmpty) {
+      setState(() {
+        _allProducts = (ModalRoute.of(context)!.settings.arguments as List<Product>).map((e) => e.clone()).toList();
+        _products = _allProducts.where((element) => element.isAdded!).toList();
+      });
+    }
   }
 
   @override
@@ -32,11 +35,52 @@ class _CartState extends State<Cart> {
           Product _product = _products[index];
           return ListTile(
             title: Text(_product.name!),
-            subtitle: Text(_product.toDescription()),
-            trailing: SumRestItemQuantity(quantity: _product.quantity!,),
+            subtitle: Text('${_product.size} ${_product.color}'),
+            trailing: SumRestItemQuantity(
+              quantity: _product.quantity!,
+              onPlusTap: () {
+                setState(() {
+                  _product.quantity = _product.quantity! + 1;
+                });
+              },
+              onMinusTap: () {
+                if(_product.quantity! > 1)
+                  setState(() {
+                    _product.quantity = _product.quantity! - 1;
+                  });
+              },
+            ),
+            leading: IconButton(
+              padding: EdgeInsets.all(2.0),
+              onPressed: () {
+                _removeFromCart(index, _product.id!);
+              },
+              icon: Icon(Icons.delete, color: Colors.red,),
+            ),
           );
         }
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _saveAll,
+        child: Icon(Icons.save),
+      ),
     );
+  }
+
+  void _removeFromCart(int index, int id) {
+    int index = _allProducts.indexWhere((element) => element.id == id);
+    setState(() {
+      _allProducts[index].isAdded = false;
+      _allProducts[index].quantity = 0;
+      _products.removeAt(index);
+    });
+  }
+
+  void _saveAll() {
+    for(Product _product in _products) {
+      int _index = _allProducts.indexWhere((element) => element.id == _product.id);
+      _allProducts[_index] = _product;
+    }
+    Navigator.pop(context, _allProducts);
   }
 }
